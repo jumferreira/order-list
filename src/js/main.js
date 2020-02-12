641c14bf0baa4e4939bdb3179f08c3a92a9b1293
Juliana Ferreira <juju_smf@hotmail.com>
2020-02-12 12:06:43 -0300

    small fixes in multilanguages and Jest added

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ import VModal from 'vue-js-modal';
 import VueI18n from 'vue-i18n';
 
 import customer from './modules/index';
+import languages from './languages.json';
 
 window.axios = _axios;
 window._ = _lodash;
@@ -25,57 +26,7 @@ Vue.use(VueLazyload);
 Vue.use(VModal);
 Vue.use(VueI18n);
 
-const messages = {
-    en: {
-        header: {
-            activateOrder: 'You have an activate order.',
-            activateOrderButton: 'Click here for details',
-        },
-        profile: {
-            salute: 'Hello, ',
-            memberSince: "You're a member since: ",
-            edit: 'edit profile',
-        },
-
-        history: {
-            title: 'Order History',
-            status: 'Status: ',
-            details: 'details',
-        },
-
-        modal: {
-            title: 'Order Detail',
-            status: 'Status: ',
-            total: 'Total: ',
-            delivery: 'Delivery: ',
-        },
-    },
-
-    ptbr: {
-        header: {
-            activateOrder: 'Você tem um pedido em aberto',
-            activateOrderButton: 'Clique aqui para mais detalhes',
-        },
-        profile: {
-            salute: 'Olá, ',
-            memberSince: "Você é um membro desde: ",
-            edit: 'editar perfil',
-        },
-
-        history: {
-            title: 'Histórico de pedidos',
-            status: 'Status: ',
-            details: 'detalhes',
-        },
-
-        modal: {
-            title: 'Detalhes do pedido',
-            status: 'Status: ',
-            total: 'Total: ',
-            delivery: 'Entrega: ',
-        },
-    }
-};
+const messages = languages;
 
 const i18n = new VueI18n({
   locale: 'en', // set locale
